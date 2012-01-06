5fccae541140aa7d96be0566c97165cc84a6fd5f
David Gouch <david@davidgouch.com>
2012-01-06 02:06:14 -0500

    Add space between anon function and parens.

--- to-title-case.js
+++ to-title-case.js
@@ -3,10 +3,10 @@
  * Copyright © 2008–2012 David Gouch. Licensed under the MIT License. 
  */
 
-String.prototype.toTitleCase = function() {
+String.prototype.toTitleCase = function () {
   var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|vs?\.?|via)$/i;
 
-  return this.replace(/([^\W_]+[^\s-]*) */g, function(match, p1, index, title) {
+  return this.replace(/([^\W_]+[^\s-]*) */g, function (match, p1, index, title) {
     if (index > 0 && index + p1.length !== title.length &&
       p1.search(smallWords) > -1 && title[index - 2] !== ":" && 
       title[index - 1].search(/[^\s-]/) < 0) {
