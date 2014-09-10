70edbe28b19b2ca815613558cbe72379007f5d1b
Toby Fox <toby@digg.com>
2014-09-10 12:06:27 -0400

    Capitalize all words following 'dash space'
    Including hyphen, en dash, and em dash

--- to-title-case.js
+++ to-title-case.js
@@ -8,7 +8,7 @@ String.prototype.toTitleCase = function(){
 
   return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
     if (index > 0 && index + match.length !== title.length &&
-      match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+      match.search(smallWords) > -1 && ! title.charAt(index - 2).match(/:|-|\u2013|\u2014/) &&
       (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
       title.charAt(index - 1).search(/[^\s-]/) < 0) {
       return match.toLowerCase();
