ffe047fbdedd8a319303dc1665d0e95918f9afa9
David Gouch <dgouch@gmail.com>
2018-07-30 18:38:44 -0400

    Format

--- test/index.js
+++ test/index.js
@@ -1,12 +1,20 @@
 require('../to-title-case.js')
-var qunit = require('qunit')
+var QUnit = require('qunit')
 var tests = require('./tests.json')
-var testGroups = ['general', 'hyphens', 'punctuation', 'quotes', 'subtitles',
-  'technical', 'versus', 'miscellaneous']
+var testGroups = [
+  'general',
+  'hyphens',
+  'punctuation',
+  'quotes',
+  'subtitles',
+  'technical',
+  'versus',
+  'miscellaneous'
+]
 
 testGroups.forEach(function (groupName) {
   tests[groupName].forEach(function (test) {
-    qunit.test(groupName, function (assert) {
+    QUnit.test(groupName, function (assert) {
       assert.equal(test.input.toTitleCase(), test.expect)
     })
   })
--- to-title-case.js
+++ to-title-case.js
@@ -1,4 +1,4 @@
-/* To Title Case | https://github.com/gouch/to-title-case | Copyright 2008–2018 David Gouch | MIT License */
+/* To Title Case © 2018 David Gouch | https://github.com/gouch/to-title-case */
 
 function toTitleCase () {
   'use strict'
@@ -6,29 +6,36 @@ function toTitleCase () {
   var alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/
   var wordSeparators = /([ :–—-])/
 
-  return this.toString().split(wordSeparators).map(function (current, index, array) {
-    /* Check for small words */
-    if (current.search(smallWords) > -1 &&
-      /* Skip first and last word */
-      index !== 0 && index !== array.length - 1 &&
-      /* Ignore title end and subtitle start */
-      array[index - 3] !== ':' && array[index + 1] !== ':' &&
-      /* Ignore small words that start a hyphenated phrase */
-      (array[index + 1] !== '-' ||
-        (array[index - 1] === '-' && array[index + 1] === '-'))) {
-      return current.toLowerCase()
-    }
+  return this.toString()
+    .split(wordSeparators)
+    .map(function (current, index, array) {
+      if (
+        /* Check for small words */
+        current.search(smallWords) > -1 &&
+        /* Skip first and last word */
+        index !== 0 &&
+        index !== array.length - 1 &&
+        /* Ignore title end and subtitle start */
+        array[index - 3] !== ':' &&
+        array[index + 1] !== ':' &&
+        /* Ignore small words that start a hyphenated phrase */
+        (array[index + 1] !== '-' ||
+          (array[index - 1] === '-' && array[index + 1] === '-'))
+      ) {
+        return current.toLowerCase()
+      }
 
-    /* Ignore intentional capitalization */
-    if (current.substr(1).search(/[A-Z]|\../) > -1) {
-      return current
-    }
+      /* Ignore intentional capitalization */
+      if (current.substr(1).search(/[A-Z]|\../) > -1) {
+        return current
+      }
 
-    /* Capitalize the first letter */
-    return current.replace(alphanumericPattern, function (match) {
-      return match.toUpperCase()
+      /* Capitalize the first letter */
+      return current.replace(alphanumericPattern, function (match) {
+        return match.toUpperCase()
+      })
     })
-  }).join('')
+    .join('')
 }
 
 // eslint-disable-next-line no-extend-native
