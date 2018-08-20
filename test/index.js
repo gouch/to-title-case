e489b6d8753ed1606b3a40bf5a5756a56e7a2ba0
David Gouch <dgouch@gmail.com>
2018-08-20 17:31:53 -0400

    Unify CLI and browser test runners

--- test/index.js
+++ test/index.js
@@ -1,21 +1,31 @@
-require('../to-title-case.js')
-var QUnit = require('qunit')
-var tests = require('./tests.json')
-var testGroups = [
-  'general',
-  'hyphens',
-  'punctuation',
-  'quotes',
-  'subtitles',
-  'technical',
-  'versus',
-  'miscellaneous'
-]
+function runTests (tests) {
+  var testGroups = [
+    'general',
+    'hyphens',
+    'punctuation',
+    'quotes',
+    'subtitles',
+    'technical',
+    'versus',
+    'miscellaneous'
+  ]
 
-testGroups.forEach(function (groupName) {
-  tests[groupName].forEach(function (test) {
-    QUnit.test(groupName, function (assert) {
-      assert.equal(test.input.toTitleCase(), test.expect)
+  testGroups.forEach(function (groupName) {
+    tests[groupName].forEach(function (test) {
+      QUnit.test(groupName, function (assert) {
+        assert.equal(test.input.toTitleCase(), test.expect)
+      })
     })
   })
-})
+}
+
+/* Distinguish between browser and node */
+if (typeof window === 'undefined') {
+  require('../to-title-case.js')
+  var QUnit = require('qunit')
+  var tests = require('./tests.json')
+  runTests(tests)
+} else {
+  /* globals jQuery */
+  jQuery.getJSON('tests.json').done(runTests)
+}
